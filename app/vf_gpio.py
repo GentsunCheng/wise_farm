71a28d772201e15210464c8728cc6f1f63585c21
GentsunCheng <oriimiku@gmail.com>
2024-05-01 22:05:56 +0800

    新更新

--- app/vf_gpio.py
+++ app/vf_gpio.py
@@ -2,6 +2,7 @@ import time
 import pymysql
 import datetime
 import threading
+import sorfcom
 from collections import namedtuple
 
 import random
@@ -13,18 +14,37 @@ class gpio_mn():
         self.datas = []
         self.delay = 0.5
         self.run = False
+        self.record_time = datetime.datetime.now()
         self.th = threading.Thread(target=self.__get_gpio__)
         self.td = threading.Thread(target=self.__write_db__)
         self.event = threading.Event()
-        self.cursor = pymysql.connect(
+        self.conn = None
+        self.cursor = None
+        self.__conn_db__()
+
+    def __del__(self):
+        self.cursor.close()
+        self.conn.close()
+        self.run = False
+
+    def __conn_db__(self):
+        self.conn = pymysql.connect(
             host="localhost",
             user="farmer",
             password="12358",
             database="wise_farm"
-        ).cursor()
+        )
+        self.cursor = self.conn.cursor()
+
+    def __clos_db__(self):
+        if self.cursor:
+            self.cursor.close()
+        if self.conn:
+            self.conn.close()
 
     def __get_gpio__(self):
         old_time = datetime.datetime.now()
+        self.record_time = old_time
         old_minute = old_time.minute
         self.td.start()
         while self.run:
@@ -38,7 +58,8 @@ class gpio_mn():
             if old_time > 56:
                 current_minute = current_minute + 60
             if current_minute - old_minute >= 3:
-                old_minute = current_minute
+                old_time = current_time
+                self.record_time = old_time.replace(minute = (old_minute + current_minute) // 2)
                 if self.td.is_alive():
                     self.event.set()
             time.sleep(self.delay)
@@ -46,20 +67,47 @@ class gpio_mn():
         return None
 
     def __write_db__(self):
+        self.__conn_db__()
+        table_name = self.record_time.strftime('%Y-%m-%d')
+        if table_exists(self.cursor, table_name):
+            print(f"表 '{table_name}' 存在")
+        else:
+            sql = ("CREATE TABLE " + table_name + " (time DATETIME, temp FLOAT,"
+                "co2 FLOAT,"
+                "light FLOAT,"
+                "CONSTRAINT check_temp CHECK (temp >= -256 AND temp <= 255),"
+                "CONSTRAINT check_co2 CHECK (co2 >= 0 AND co2 <= 500),"
+                "CONSTRAINT check_light CHECK (light >= 0 AND light <= 100)"
+                ")")
+            try:
+                self.cursor.execute(sql)
+                self.conn.commit()
+                print("创建成功")
+            except Exception as e:
+                self.conn.rollback()
+                print("创建失败失败:", e)
+        self.__clos_db__()
         while self.run:
             self.event.wait()
-            sql = "INSERT INTO your_table (column1, column2) VALUES (%s, %s)"
-            data = ("value1", "value2")
+            self.__conn_db__()
+            tmp_list = []
+            tmp_list.append(self.record_time.strftime('%Y-%m-%d %H:%M'))
+            for i in range(3):
+                tmp = 0
+                for j in self.datas:
+                    tmp = tmp + self.datas[j][i]
+                tmp_list.append(tmp)
+            sql = "INSERT INTO your_table (time, temp, co2, light) VALUES (%s, %s, %s, %s)"
+            data = tuple(tmp_list)
+            tmp_list.clear()
             try:
-                cursor.execute(sql, data)
-                conn.commit()
+                self.cursor.execute(sql, data)
+                self.conn.commit()
                 print("数据插入成功")
             except Exception as e:
-                conn.rollback()
+                self.conn.rollback()
                 print("数据插入失败:", e)
-            finally:
-                cursor.close()
-                conn.close()
+            self.__clos_db__()
             self.datas.clear()
             self.event.clear()
 
@@ -114,4 +162,3 @@ class gpio_mn():
 
         else:
             return None
-
